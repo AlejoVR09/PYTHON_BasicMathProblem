5d2abca351fa375f9914265748b4ef260d1a3e56
Alejo <Alejandrovanegas09@outlook.com>
2023-07-05 10:12:06 -0500

    English documentation

--- Derivadas.py
+++ Derivadas.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+"""
+This function is the representation of the quadratic math function, it recieve as parameter a set of values call "X" what
+are meant to be evaluated
+Esta funcion es la representacion de una funcion cuadratica, que recibe como parametro en conjunto de valores X que sera
+evaluado
+"""
 def f(x):
   return x**2
 
+"""
+This function is the representation of the derivate of f(x) that recieve as parameter the math function,
+in its body we have h what represent the approaching value of the function to 0 (limit)
+Esta funcion es la representacion de la derivada de f(X) que recibe como parametro la funcion,
+en el cuerpo de la tenemos a h que es el valor al que se aproxima la funcion (limite)
+"""
 def dy(f):
   h=0.00000000001
   return (f(x+h)-f(x))/h
@@ -13,5 +26,5 @@ y=f(x)
 dy=dy(f)
 
 plt.grid()
-plt.plot(x,dy,'r')
 plt.plot(x,y,'b')
+plt.plot(x,dy,'r')
--- MaxMin.py
+++ MaxMin.py
@@ -1,10 +1,23 @@
 from sympy import * 
+
+"""
+This function refers to a more legible represetation using a tiposetter
+x represents the math's variable X
+expr represents an expression
+"""
 init_printing( use_latex='mathjax' )
 x = Symbol('x')
 expr = x**4 - 3*x**2
-# ^^ esta es nuestra expresion
+
+"""
+expr_d represents the derivation of the expression
+Eq is a function to make a equation equal to any value for resolving it
+"""
 expr_d = Derivative(expr, x, evaluate=True)
-# ^^ esta es la derivada de nuestra expresion
 Eq(expr_d, 0)
 
+
+"""
+solve is use for resolving equations
+"""
 solve(Eq(expr_d, 0))
